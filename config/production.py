dba99a07b85448f69ad347dd6429aa66c2d4c55f
김찬영 <cykimtt@gmail.com>
2020-12-27 23:14:38 +0900

    DB 교체

--- config/production.py
+++ config/production.py
@@ -1,7 +1,13 @@
 from config.default import *
 from logging.config import dictConfig
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user='dbmasteruser',
+    pw='vERYlONGpASSWORD!!!',
+    url='ls-89ba7ee2fba281bddc1a25ebc3a1ad4a3332159f.cordcckkaqsz.ap-northeast-2.rds.amazonaws.com',
+    db='flask_pybo')
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xd5\xb4\x95\xd2\x12\xbb\x94\xa23yw\xf7\x8e7\xd5q'
 
